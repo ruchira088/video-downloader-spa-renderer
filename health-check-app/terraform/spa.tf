2499db2082dfdcf101a0e5663755d1ef94db87a5
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-07-01 02:13:39 +1000

    Updating Terraform files

--- health-check-app/terraform/spa.tf
+++ health-check-app/terraform/spa.tf
@@ -7,19 +7,6 @@ resource "aws_s3_bucket_acl" "spa_bucket_acl" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket_website_configuration" "spa_website" {
-  bucket = aws_s3_bucket.spa_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-
-  }
-}
-
 resource "aws_s3_bucket_policy" "spa_bucket_policy" {
   bucket = aws_s3_bucket.spa_bucket.bucket
   policy = jsonencode({
@@ -55,10 +42,11 @@ resource "aws_cloudfront_distribution" "spa_cf" {
   enabled = true
 
   aliases = [ var.domain_name ]
+  default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods        = ["HEAD", "GET", "OPTIONS"]
-    cached_methods         = ["HEAD", "GET", "OPTIONS"]
+    allowed_methods        = ["HEAD", "GET"]
+    cached_methods         = ["HEAD", "GET"]
     target_origin_id       = aws_s3_bucket.spa_bucket.id
     viewer_protocol_policy = "redirect-to-https"
 
@@ -66,27 +54,20 @@ resource "aws_cloudfront_distribution" "spa_cf" {
       query_string = true
 
       cookies {
-        forward = "none"
+        forward = "all"
       }
     }
   }
 
   custom_error_response {
-    error_code = 404
+    error_code = 403
     response_code = 200
-    response_page_path = "/"
+    response_page_path = "/index.html"
   }
 
   origin {
-    domain_name = aws_s3_bucket.spa_bucket.website_endpoint
+    domain_name = aws_s3_bucket.spa_bucket.bucket_domain_name
     origin_id   = aws_s3_bucket.spa_bucket.id
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
   }
 
   restrictions {
