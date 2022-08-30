f40bffafe53195567baa8c9517d0552243168978
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-08-30 20:57:30 +1000

    Using jsdom to query HTML document in app.test.ts

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     testPathIgnorePatterns: [ "/health-check-app/" ],
-    testTimeout: 20_000
+    testTimeout: 25_000
 }
--- src/app.test.ts
+++ src/app.test.ts
@@ -4,6 +4,7 @@ import {HEALTH_CHECK_READY_CSS_SELECTORS, HEALTH_CHECK_URL} from "./services/Hea
 import {defaultClock} from "./utils/Clock"
 import {applicationConfiguration} from "./config/ApplicationConfiguration"
 import {launchBrowser} from "./services/RenderingService"
+import jsdom from "jsdom"
 
 describe("Testing HTTP application", () => {
     it("Retrieving the HTML markup of the health check SPA service", async () => {
@@ -15,12 +16,15 @@ describe("Testing HTTP application", () => {
                 .post("/render")
                 .send({url: HEALTH_CHECK_URL, readyCssSelectors: HEALTH_CHECK_READY_CSS_SELECTORS})
 
-        const expectedHead = `<meta charset="utf-8"><link rel="icon" href="/favicon.ico"><meta name="viewport" content="width=device-width,initial-scale=1"><meta name="theme-color" content="#000000"><meta name="description" content="SPA for the video-downloader-spa-renderer health check"><link rel="apple-touch-icon" href="/logo192.png"><title>React App</title>\x3Cscript defer="defer" src="/static/js/main.b7909c77.js">\x3C/script><link href="/static/css/main.7aa06142.css" rel="stylesheet">`
-        const expectedBody = `<noscript>You need to enable JavaScript to run this app.</noscript><div id="root"><div id="app"><div class="header"><h1>Test Page</h1></div><div class="content"><div id="text-field">ID specified</div><div class="class-name">Class specified</div><div class="deferred-class-name">Hello World</div></div></div></div>`
+        await browser.close()
 
         expect(response.status).toBe(200)
-        expect(response.text).toBe(`<!DOCTYPE html><html lang="en"><head>${expectedHead}</head><body>${expectedBody}</body></html>`)
 
-        await browser.close()
+        const {document} = new jsdom.JSDOM(response.text).window
+
+        expect(document.getElementById("text-field")?.textContent).toBe("ID specified")
+        expect(document.querySelector(".class-name")?.textContent).toBe("Class specified")
+        expect(document.querySelector(".deferred-class-name")?.textContent).toBe("Hello World")
+        expect(document.getElementById("build-timestamp")).toBeTruthy()
     })
 })
